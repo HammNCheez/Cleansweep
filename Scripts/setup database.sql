9ba89d782adfe2492dd5d85348218cac9dfe426d
Jonathan.Hammond <Jonathan.Hammond@SunGard.com>
2014-05-08 22:19:50 -0500

    Create database setup script.

---
+++ Scripts/setup database.sql
@@ -0,0 +1,17 @@
+create database cleansweep;
+use cleansweep;
+
+create table tasks (id serial primary key, task_name varchar(100) not null, 
+description varchar(255) not null, cost decimal(6,2) not null, duration smallint not null);
+
+create table scheduled_tasks (id serial primary key, task_id bigint unsigned not null, 
+scheduled_date date not null, completed_date datetime, reviewed_date datetime,
+assignee_id bigint unsigned, reviewer_id bigint unsigned);
+
+create table reoccuring_tasks (id serial primary key, task_id bigint unsigned not null,
+frequency char(1) not null, day_number smallint, task_interval smallint, week_of_month smallint,
+end_date date, occurances smallint, start_date date not null);
+
+create table users (id serial primary key, first_name varchar(50) not null, 
+last_name varchar(50) not null, username varchar(30) not null, password_hash char(64) not null,
+access_level smallint not null, allowance_total decimal(6,2));
