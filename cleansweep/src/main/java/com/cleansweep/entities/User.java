cd2546a202c4329d62bac3184e8520bcd9366408
Jonathan.Hammond <Jonathan.Hammond@SunGard.com>
2014-05-27 17:26:40 -0500

    Added salt to user object

--- cleansweep/src/main/java/com/cleansweep/entities/User.java
+++ cleansweep/src/main/java/com/cleansweep/entities/User.java
@@ -29,6 +29,9 @@ public class User {
 	@Column(name = "password_hash")
 	private String password;
 
+	@Column(name = "salt")
+	private String salt;
+
 	@Column(name = "access_level")
 	private int accessLevel;
 
@@ -73,6 +76,14 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
 	}
 
 	public int getAccessLevel() {
